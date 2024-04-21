a2cc3deb5c0ff25a61f874743197a48d29dbb5c9
Federico De Felici <fdf8210@gmail.com>
2024-04-21 10:54:40 +0200

    #100 add temporary test

--- tests/functs/src/unit/report_pretty_full_test.c
+++ tests/functs/src/unit/report_pretty_full_test.c
@@ -67,6 +67,35 @@ CLOVE_TEST(ReportOneSuiteWithOnePassedTest) {
     CLOVE_STRING_EQ(expected, actual);
 }
 
+CLOVE_TEST(ReportWithTestNameLongerThanSuiteName) {
+    __clove_suite_t suite = create_suite("bl_blitters");
+    __clove_test_t test11 = create_test("bl_blitters_select_color__blit_color");
+    //bl_blitters.bl_blitters_select_color__blit_color_blend
+
+    suite_add_test(&suite, &test11);
+    __CLOVE_VECTOR_ADD(&suites, __clove_suite_t, suite);
+   
+    __clove_report_t* base = (__clove_report_t*)report;
+    base->start(base, &suites, 1);
+    base->begin_suite(base, &suite, 0);
+    base->end_test(base, &suite, &test11, 1);
+    base->end_suite(base, &suite, 0);
+    base->end(base, 1, 0, 0);
+
+    const char* expected = 
+    "[INFO] Executing Test Runner with detail level: 'Full'\n"
+    "[INFO] Suites / Tests found: 1 / 1\n"
+    "[INFO] 1) bl_blitters.bl_blitters_select_color__blit_color..[PASS] (0.000 ms)\n"
+    "[INFO] Total: 1, Passed: 1, Failed: 0, Skipped: 0\n"
+    "[INFO] Run duration: 0 ms\n"
+    "[INFO] Run result: SUCCESS :-)\n"
+    ;
+
+    char* actual = __clove_stream_memory_as_string(stream);
+
+    CLOVE_STRING_EQ(expected, actual);
+}
+
 CLOVE_TEST(ReportOneSuiteWithTwoTests) {
     __clove_suite_t suite = create_suite("Suite1");
     __clove_test_t test11 = create_test("Test11");
